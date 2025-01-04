bb81f8924e177a80bd4d7d733c1a727effb4381a
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2025-01-04 07:48:34 -0500

    update copyright year

--- melpaca.el
+++ melpaca.el
@@ -1,6 +1,6 @@
 ;;; melpaca.el --- CI tool for MELPA  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024  Nicholas Vollmer
+;; Copyright (C) 2024-2025  Nicholas Vollmer
 
 ;; Author: Nicholas Vollmer) <nv@parenthetic.dev>
 ;; Keywords: convenience, tools
